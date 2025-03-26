fdf3b8548f418e8a7fa8cde1bd5cdbf69c538798
Giada <giada.bellan@live.it>
2025-03-26 18:36:23 +0100

    progress on favorites

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,32 +2,47 @@ import { Link } from "react-router";
 import pokeballLogo from "../assets/pokeball_logo.png";
 import favHeartSelected from "../assets/favheart_selected.png";
 
-
 // This component simply renders a navigation bar
 const Navbar = () => {
   return (
     <nav className="bg-gray-600 p-2 mb-4 flex flex-row justify-between sticky top-0 z-20 shadow-md">
-
       <Link to="/">
         <div className="pt-0.5 mx-3 flex flex-row">
-          <img src={pokeballLogo} alt="Favorites icon" className="w-[30px] mr-2 "/>
-          <div className=" self-center text-white font-bold ">
-            My Pokédex
-          </div>
+          <img
+            src={pokeballLogo}
+            alt="Favorites icon"
+            className="w-[30px] mr-2 "
+          />
+          <div className=" self-center text-white font-bold ">My Pokédex</div>
         </div>
       </Link>
 
       <div className="flex">
-        <div className="flex border-2 border-gray-600 rounded-xl focus-within:border-red-500">
-
-          <input id="searchInput"
-            className=" bg-gray-100 border-none focus:outline-none focus:ring-0 focus:border-transparent h-[30px] rounded-bl-xl rounded-tl-xl self-center text-center"
+        <div className="flex border-3 border-gray-900 rounded-xl focus-within:border-gray-400">
+          <input
+            id="searchInput"
+            className=" bg-gray-100 border-none focus:outline-none focus:ring-0 focus:border-transparent h-[30px] rounded-bl-lg rounded-tl-lg self-center text-center"
             type="text"
             placeholder="Search by name or ID"
           />
-          <button id="searchButton" type="submit" className="bg-gray-600 rounded-br-lg rounded-tr-lg text-white  hover:border-gray-600 hover:border-l-red-500 hover:bg-red-500">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M7 19a8 8 9 100-18 9 9 0 000 18zm8-1l10 10"></path>
+          <button
+            id="searchButton"
+            type="submit"
+            className="bg-gray-600 rounded-br-lg rounded-tr-lg text-white  hover:border-gray-600 hover:border-l-gray-500 hover:bg-gray-500"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="4"
+                d="M7 19a8 8 9 100-18 9 9 0 000 18zm8-1l10 10"
+              ></path>
             </svg>
           </button>
         </div>
@@ -35,10 +50,12 @@ const Navbar = () => {
 
       <Link to="/favorites">
         <div className="pt-0.5 mx-4 flex flex-row">
-          <img src={favHeartSelected} alt="Favorites icon" className="w-[30px] ml-4 mr-2"/>
-          <div className=" self-center text-white font-bold ">
-            Favorites
-          </div>
+          <img
+            src={favHeartSelected}
+            alt="Favorites icon"
+            className="w-[30px] ml-4 mr-2"
+          />
+          <div className=" self-center text-white font-bold ">Favorites</div>
         </div>
       </Link>
     </nav>
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,91 @@
-const Favorites = () => {
-    return <div>Favorites</div>;
-  };
+import { useState } from "react";
+import { Link } from "react-router";
+import { useFavePoke } from "../hooks/useFavePoke";
+import favHeartUnselected from "../assets/favheart_unselected.png";
+import favHeartHovered from "../assets/favheart_hovered.png";
+import favHeartSelected from "../assets/favheart_selected.png";
 
-  export default Favorites;
+const Favorites = () => {
+  const { favorites, addToFavorites } = useFavePoke();
+
+  return (
+    <div
+      id="favorite-container"
+      className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-4"
+    >
+      {favorites.map((pokemon) => {
+        const isFavorite = favorites.some((fav) => fav.id === pokemon.id);
+        const [hovered, setHovered] = useState(false);
+
+        const handleMouseEnter = () => {
+          if (!isFavorite) {
+            setHovered(true);
+          }
+        };
+
+        const handleMouseLeave = () => {
+          if (!isFavorite) {
+            setHovered(false);
+          }
+        };
+
+        const favBtnState = isFavorite
+          ? favHeartSelected
+          : hovered
+          ? favHeartHovered
+          : favHeartUnselected;
+
+        return (
+          <div
+            key={pokemon.id}
+            className="rounded-2xl shadow-md px-4 flex flex-col items-center text-center border-gray-400 border-solid border-2 hover:cursor-pointer group"
+          >
+            <Link to={`/pokemon/${pokemon.id}`}>
+              <h2 className="text-xl font-bold bg-white rounded-lg w-[95%] py-1 mt-2 z-10">
+                {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+              </h2>
+              <div className="overflow-hidden">
+                <img
+                  className="rounded-lg p-4 transition duration-300 group-hover:scale-110 z-0"
+                  src={pokemon.image || "placeholder.png"}
+                  alt={pokemon.name}
+                />
+              </div>
+            </Link>
+            <div className="flex flex-row w-full">
+              <Link to={`/pokemon/${pokemon.id}`}>
+                <p className="text-gray-900 rounded-lg flex-grow flex-1 px-4 z-10">
+                  # {String(pokemon.id).padStart(3, "0")}
+                </p>
+              </Link>
+
+              <Link to={`/pokemon/${pokemon.id}`} className="flex-grow">
+                <div className="p-2"></div>
+              </Link>
+
+              <div className="flex flex-row">
+                <button
+                  onClick={() => addToFavorites(pokemon)}
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                  className="w-[20px] z-30 cursor-pointer"
+                >
+                  <img src={favBtnState} alt="Favorite button" />
+                </button>
+
+                <Link to={`/pokemon/${pokemon.id}`}>
+                  <div className="p-2"></div>
+                </Link>
+              </div>
+            </div>
+            <Link to={`/pokemon/${pokemon.id}`} className="w-full">
+              <div className="p-2"></div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Favorites;
