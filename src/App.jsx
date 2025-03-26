ceb6c4bcc3fa9b5ddf04f330b1d78959b55df718
Giada <giada.bellan@live.it>
2025-03-26 13:31:56 +0100

    add footer, assets and favorites button + page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Link, Route, Routes } from "react-router";
 import MainLayout from "./layout/Mainlayout.jsx";
 import Home from "./pages/Home.jsx";
 import Details from "./pages/Details.jsx";
+import Favorites from "./pages/Favorites.jsx";
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="pokemon/:id" element={<Details />} />
+          <Route path="favorites" element={<Favorites />}/>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router";
+
+// This component simply renders a navigation bar
+const Footer = () => {
+  return (
+    <footer class="flex bg-gray-200 mt-4 h-[50px] bottom-0 z-10 w-full">    
+      <p class="pl-3 self-center">
+        &copy; PokéLove Inc. 💕
+      </p>  
+    </footer>
+  );
+};
+
+export default Footer;
+
